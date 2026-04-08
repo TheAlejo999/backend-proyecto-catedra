d9406e2300792de5639639c9cbf0718dd8053d54
Reina <reinasosa2024@gmail.com>
2026-04-08 09:05:43 -0600

    fuelSupply

---
+++ app/Http/Requests/UpdateFuelSupplyRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateFuelSupplyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'vehicle_id' => ['exists:vehicles,id'],
+            'route_id' => ['exists:routes,id'],
+            'amount_gallons' => ['numeric', 'min:0.01', 'regex:/^\d{1,6}(\.\d{1,2})?$/'],
+            'total_cost' => ['numeric', 'min:0.01', 'regex:/^\d{1,8}(\.\d{1,2})?$/'],
+            'date' => ['date', 'before_or_equal:today']
+        ];
+    }
+}
